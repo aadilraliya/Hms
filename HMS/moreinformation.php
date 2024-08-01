60990753b529720e9ae0d377eb8371301535000a
Mohammad Aadil Raliya <153985636+aadilraliya@users.noreply.github.com>
2024-08-01 16:42:08 +0530

    Add files via upload

---
+++ HMS/moreinformation.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>More information</title>
+</head>
+<body style="background-image: url(img/back3.jpg); background-repeat: no-repeat; background-size: cover;">
+
+<?php
+    include("include/header.php");
+ ?>
+ <div class="container-fluid">
+    <div class="col-md-12">
+        <div class="row">
+            <div class="col-md-2"></div>
+            <div class="col-md-8 jumbotron my-3">
+                <h5 class="text-center">More Information</h5><br>
+                
+                <h3 class="text-center"><p>Information about Supeda Surgical & Maternity Hospital in Panchmahal, Gujarat. The information includes contact details like phone numbers, emails, contact person and directions to hospital or clinic!!</p></h3><br>
+
+                <h4 class="text-center"><b><p>Address : Derol Station Road, Kalol,</p>
+                                           <p> Panchmahal - 389330,</p>
+                                            <p>Gujarat</p></b></h4><br><br>
+
+                <h4 class=text-center><b><p>Hours: Open 24x7 hours/days</p></b></h4><br>
+                <h4 class=text-center><b><p>Phone No: 088666 63536</p></b></h4>
+
+
+            </div>
+            <div class="col-md-2"></div>
+        </div>
+    </div>
+ </div>
+    
+</body>
+</html>
